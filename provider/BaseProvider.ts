13066c6e7e4608388e3b56ee1b7e0498c6001c79
Rodrigo Orem <orem.rodrigo@gmail.com>
2021-03-23 09:58:39 -0300

    Move zip code from webpack to the provider

--- provider/BaseProvider.ts
+++ provider/BaseProvider.ts
@@ -1,3 +1,5 @@
+import yazl from 'yazl'
+
 export type Functions = Record<string, { url: string }>
 
 export abstract class BaseProvider {
@@ -8,4 +10,18 @@ export abstract class BaseProvider {
   }
 
   public abstract getOrCreateFunction(functionName: string, content: Buffer): Promise<string | undefined>
+
+  public async zipFunction(content: Buffer): Promise<Buffer> {
+    const zipfile = new yazl.ZipFile()
+    zipfile.addBuffer(content, 'index.js')
+
+    return new Promise((resolve, reject) => {
+      const data = []
+
+      zipfile.outputStream.on('data', (chunk) => data.push(chunk))
+      zipfile.outputStream.on('end', () => resolve(Buffer.concat(data)))
+      zipfile.outputStream.on('error', (error) => reject(error))
+      zipfile.end()
+    })
+  }
 }
